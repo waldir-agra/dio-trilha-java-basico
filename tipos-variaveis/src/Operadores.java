8405f11b59dc212f52d2577f4cfea453ce793105
waldir-agra <waldiragra@gmail.com>
2024-06-03 22:56:45 -0300

    projeto conta-banco DIO

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+    
+    public static void main (String[]args){
+
+        String nomeCompleto = "linguagem " + "java";
+
+        System.out.println(nomeCompleto);
+    }
+    
+
+
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      //tipos primitivos
+      //estudem a classe string que representa texto na aplicação
+      String meuNome = "Waldir_agra";
+
+
+      double salarioMinimo = 2500;
+
+      short numeroCurto = 1;
+      int numeroNormal = numeroCurto;
+      short numeroCurto2 = (short) numeroNormal;
+
+      int numero = 5;
+      numero = 10;
+      
+      
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
